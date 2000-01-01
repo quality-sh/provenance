c9d9368bd7a9fb1b5a687baa06c78846f24ca3cd
Provenance CI snapshot <agent-ci@users.noreply.github.com>
2000-01-01 00:00:00 +0000

    Provenance CI snapshot

--- crates/provenance-transport/tests/porcelain_search.rs
+++ crates/provenance-transport/tests/porcelain_search.rs
@@ -4,8 +4,7 @@ mod support {
     pub mod records;
 }
 
-use provenance_core::NodeType;
-use provenance_store::operations::catalog;
+use provenance_store::operations::catalog::{self, Operation as _};
 use provenance_transport::{
     fixture::{FixtureAccess, Target},
     StatementHost,
@@ -27,18 +26,23 @@ fn access(repository: &Repository) -> FixtureAccess {
     .unwrap()
 }
 
-fn deny_kind(mut access: FixtureAccess, kind: NodeType) -> FixtureAccess {
-    for definition in
-        catalog::definitions().iter().filter(|definition| {
-            definition.registration.queries.iter().any(|query| {
-                query.name == "trace" && query.request.node_type == Some(kind.as_str())
-            })
-        })
-    {
-        access = access.deny_operation(definition.name);
+fn deny_operations(mut access: FixtureAccess, operations: &[&str]) -> FixtureAccess {
+    for operation in operations {
+        access = access.deny_operation(operation);
     }
     access
 }
+
+const LIST_SEARCH_OPERATIONS: [&str; 8] = [
+    "list-sources",
+    "list-requirements",
+    "list-resolutions",
+    "list-rules",
+    "list-topics",
+    "list-questions",
+    "list-domains",
+    "list-boundaries",
+];
 
 async fn call(
     client: &rmcp::service::RunningService<rmcp::RoleClient, ()>,
@@ -51,6 +55,17 @@ async fn call(
         )
         .await
         .unwrap()
+}
+
+async fn assert_search_hidden_and_refused(
+    client: &rmcp::service::RunningService<rmcp::RoleClient, ()>,
+) {
+    let tools = client.list_all_tools().await.unwrap();
+    assert!(!tools.iter().any(|tool| tool.name == "search"));
+    let direct = client
+        .call_tool(CallToolRequestParams::new("search"))
+        .await;
+    assert!(direct.is_err(), "{direct:?}");
 }
 
 #[tokio::test]
@@ -154,39 +169,114 @@ async fn mcp_search_content_matches_the_structured_bounded_page() {
 }
 
 #[tokio::test]
-async fn mcp_search_applies_kind_grants_before_the_canonical_page() {
+async fn list_denial_excludes_a_kind_before_selection_and_paging() {
     let repository = Repository::new("The shared graph is searchable.");
     repository.all_kinds();
-    let restricted = deny_kind(access(&repository), NodeType::Source);
+    let restricted = access(&repository).deny_operation("list-sources");
     let host = StatementHost::with_fixture_access(restricted);
     let (client_io, server_io) = tokio::io::duplex(256 * 1024);
     let server = tokio::spawn(async move { host.serve_mcp(server_io).await.unwrap() });
     let client = ().serve(client_io).await.unwrap();
 
-    let result = call(&client, json!({"node_types":["source", "rule"], "limit":1})).await;
-    let structured = result.structured_content.unwrap();
-    assert_eq!(structured["nodes"][0]["node_type"], "rule");
-    assert_eq!(structured["has_more"], false);
-    assert!(structured["next_cursor"].is_null());
+    let source_only = call(&client, json!({"node_types":["source"]})).await;
+    assert_eq!(source_only.is_error, Some(true), "{source_only:?}");
+    assert_eq!(
+        source_only.structured_content.as_ref().unwrap()["error"]["kind"],
+        "access_denied"
+    );
+
+    let first = call(
+        &client,
+        json!({"node_types":["source", "requirement", "rule"], "limit":1}),
+    )
+    .await;
+    let first_page = first.structured_content.as_ref().unwrap();
+    assert_eq!(first_page["nodes"].as_array().unwrap().len(), 1);
+    assert_eq!(first_page["nodes"][0]["node_type"], "requirement");
+    assert_eq!(first_page["has_more"], true);
+    let cursor = first_page["next_cursor"].as_str().unwrap();
+    assert!(!serde_json::to_string(&first).unwrap().contains("source_shared"));
+
+    let second = call(
+        &client,
+        json!({
+            "node_types":["source", "requirement", "rule"],
+            "limit":1,
+            "cursor":cursor
+        }),
+    )
+    .await;
+    let second_page = second.structured_content.as_ref().unwrap();
+    assert_eq!(second_page["nodes"].as_array().unwrap().len(), 1);
+    assert_eq!(second_page["nodes"][0]["node_type"], "rule");
+    assert_eq!(second_page["has_more"], false);
+    assert!(second_page["next_cursor"].is_null());
+    assert!(!serde_json::to_string(&second).unwrap().contains("source_shared"));
+
+    let implicit = call(&client, json!({"text":"shared", "limit":20})).await;
+    let implicit_page = implicit.structured_content.as_ref().unwrap();
+    assert_eq!(implicit_page["nodes"].as_array().unwrap().len(), 7);
+    assert!(implicit_page["nodes"]
+        .as_array()
+        .unwrap()
+        .iter()
+        .all(|node| node["node_type"] != "source"));
+    assert_eq!(implicit_page["has_more"], false);
+    assert!(implicit_page["next_cursor"].is_null());
+    assert!(!serde_json::to_string(&implicit)
+        .unwrap()
+        .contains("source_shared"));
 
     client.cancel().await.unwrap();
     server.await.unwrap().cancel().await.unwrap();
 }
 
 #[tokio::test]
-async fn mcp_hides_search_when_no_record_kind_is_permitted() {
+async fn list_grant_allows_search_when_get_is_denied() {
     let repository = Repository::new("The shared graph is searchable.");
-    let mut restricted = access(&repository);
-    for kind in NodeType::ALL {
-        restricted = deny_kind(restricted, kind);
-    }
+    repository.all_kinds();
+    let restricted = access(&repository).deny_operation("get-source");
     let host = StatementHost::with_fixture_access(restricted);
     let (client_io, server_io) = tokio::io::duplex(256 * 1024);
     let server = tokio::spawn(async move { host.serve_mcp(server_io).await.unwrap() });
     let client = ().serve(client_io).await.unwrap();
 
-    let tools = client.list_all_tools().await.unwrap();
-    assert!(!tools.iter().any(|tool| tool.name == "search"));
+    let result = call(&client, json!({"node_types":["source"]})).await;
+    assert_ne!(result.is_error, Some(true), "{result:?}");
+    assert_eq!(
+        result.structured_content.as_ref().unwrap()["nodes"][0]["id"],
+        "source_shared"
+    );
+
+    client.cancel().await.unwrap();
+    server.await.unwrap().cancel().await.unwrap();
+}
+
+#[tokio::test]
+async fn no_list_search_grant_hides_and_refuses_named_search() {
+    let repository = Repository::new("The shared graph is searchable.");
+    let restricted = deny_operations(access(&repository), &LIST_SEARCH_OPERATIONS);
+    let host = StatementHost::with_fixture_access(restricted);
+    let (client_io, server_io) = tokio::io::duplex(256 * 1024);
+    let server = tokio::spawn(async move { host.serve_mcp(server_io).await.unwrap() });
+    let client = ().serve(client_io).await.unwrap();
+
+    assert_search_hidden_and_refused(&client).await;
+
+    client.cancel().await.unwrap();
+    server.await.unwrap().cancel().await.unwrap();
+}
+
+#[tokio::test]
+async fn global_search_denial_hides_and_refuses_named_search() {
+    let repository = Repository::new("The shared graph is searchable.");
+    let restricted = access(&repository).deny_operation(catalog::Search::NAME);
+    let host = StatementHost::with_fixture_access(restricted);
+    let (client_io, server_io) = tokio::io::duplex(256 * 1024);
+    let server = tokio::spawn(async move { host.serve_mcp(server_io).await.unwrap() });
+    let client = ().serve(client_io).await.unwrap();
+
+    assert_search_hidden_and_refused(&client).await;
 
     client.cancel().await.unwrap();
     server.await.unwrap().cancel().await.unwrap();
