5c92dcb4eb95f766f183c9e8a8dd2fc26dd027ab
Provenance CI snapshot <agent-ci@users.noreply.github.com>
2000-01-01 00:00:00 +0000

    Provenance CI snapshot

--- crates/provenance-porcelain/tests/search.rs
+++ crates/provenance-porcelain/tests/search.rs
@@ -2,12 +2,46 @@ use provenance_core::protocol::{
     GraphNode, QueryResponse, SearchQuery, SearchResult, Stamp, StampPolicy,
 };
 use provenance_core::NodeType;
-use provenance_porcelain::search::{PortFuture, SearchError, SearchPort};
+use provenance_porcelain::search::{render_readable, PortFuture, SearchError, SearchPort};
 use provenance_porcelain::Porcelain;
 use std::sync::{Arc, Mutex};
 
 #[derive(Clone, Default)]
 struct RecordingPort(Arc<Mutex<Option<SearchQuery>>>);
+
+fn response(
+    nodes: Vec<GraphNode>,
+    limit: usize,
+    has_more: bool,
+    next_cursor: Option<&str>,
+    freshness_error: Option<&str>,
+) -> QueryResponse<SearchResult> {
+    QueryResponse {
+        protocol_version: provenance_core::SDK_PROTOCOL_VERSION,
+        operation: "search",
+        stamp: Stamp {
+            serial: 1,
+            digest: "sha256:fixture".into(),
+            instance_id: "fixture".into(),
+            derivation: 1,
+            policy: StampPolicy::AnnotateOnly,
+            attested: vec!["rules".into()],
+            live: Vec::new(),
+        },
+        freshness_error: freshness_error.map(str::to_owned),
+        freshness_cause: None,
+        result: SearchResult {
+            next_cursor: next_cursor.map(str::to_owned),
+            limit,
+            has_more,
+            nodes,
+        },
+    }
+}
+
+fn node(value: serde_json::Value) -> GraphNode {
+    serde_json::from_value(value).unwrap()
+}
 
 impl SearchPort for RecordingPort {
     fn search(&self, request: SearchQuery) -> PortFuture<'_> {
@@ -77,4 +111,46 @@ async fn search_refuses_a_request_without_text_or_kinds_before_the_port() {
         Err(SearchError::InvalidOptions)
     ));
     assert!(port.0.lock().unwrap().is_none());
+}
+
+#[test]
+fn readable_search_renders_an_empty_bounded_page() {
+    let page = response(Vec::new(), 50, false, None, None);
+
+    assert_eq!(
+        render_readable(&page),
+        "search: 0 returned\nbounds: limit=50 has_more=false continuation=none"
+    );
+}
+
+#[test]
+fn readable_search_renders_records_continuation_and_freshness() {
+    let page = response(
+        vec![
+            node(serde_json::json!({
+                "node_type": "requirement", "schema_version": 2, "scope_id": "default",
+                "id": "req_one", "statement": "First requirement statement.",
+                "status": "active"
+            })),
+            node(serde_json::json!({
+                "node_type": "source", "schema_version": 2, "scope_id": "default",
+                "id": "source_two", "name": "Source archive", "source_type": "document"
+            })),
+        ],
+        2,
+        true,
+        Some("cursor-two"),
+        Some("projection is stale"),
+    );
+
+    assert_eq!(
+        render_readable(&page),
+        concat!(
+            "search: 2 returned\n",
+            "- requirement req_one\n  First requirement statement.\n",
+            "- source source_two\n  Source archive\n",
+            "bounds: limit=2 has_more=true continuation=cursor-two\n",
+            "warning: freshness: projection is stale"
+        )
+    );
 }
