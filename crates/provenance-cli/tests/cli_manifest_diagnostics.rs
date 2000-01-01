da4fffd605cd0979ba419566a330e3036389462a
Provenance CI snapshot <agent-ci@users.noreply.github.com>
2000-01-01 00:00:00 +0000

    Provenance CI snapshot

--- crates/provenance-cli/tests/cli_manifest_diagnostics.rs
+++ crates/provenance-cli/tests/cli_manifest_diagnostics.rs
@@ -28,3 +28,32 @@ fn requirements_list_identifies_the_selected_malformed_manifest() {
     assert!(!selected.join(".provenance/cache/provenance.db").exists());
     assert!(!selected.join(".provenance/state/scopes").exists());
 }
+
+#[test]
+fn init_identifies_the_selected_malformed_manifest() {
+    let temporary = tempfile::tempdir().unwrap();
+    let selected = temporary.path().join("selected");
+    let manifest = selected.join(".provenance/state/manifest.json");
+    std::fs::create_dir_all(manifest.parent().unwrap()).unwrap();
+    let malformed = b"{bad";
+    std::fs::write(&manifest, malformed).unwrap();
+
+    let output = Command::cargo_bin("provenance")
+        .unwrap()
+        .current_dir(temporary.path())
+        .args(["init", "--path", selected.to_str().unwrap()])
+        .output()
+        .unwrap();
+
+    assert!(!output.status.success());
+    assert!(output.stdout.is_empty());
+    let stderr = String::from_utf8(output.stderr).unwrap();
+    assert!(stderr.contains(manifest.to_str().unwrap()), "{stderr}");
+    assert!(
+        stderr.contains("key must be a string at line 1 column 2"),
+        "{stderr}"
+    );
+    assert_eq!(std::fs::read(&manifest).unwrap(), malformed);
+    assert!(!selected.join(".provenance/cache/provenance.db").exists());
+    assert!(!selected.join(".provenance/state/scopes").exists());
+}
