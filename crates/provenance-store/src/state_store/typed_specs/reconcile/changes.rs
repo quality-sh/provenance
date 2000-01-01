0dc96027d61468c769ca78a663a940ffd0098571
Provenance CI snapshot <agent-ci@users.noreply.github.com>
2000-01-01 00:00:00 +0000

    Provenance CI snapshot

--- crates/provenance-store/src/state_store/typed_specs/reconcile/changes.rs
+++ crates/provenance-store/src/state_store/typed_specs/reconcile/changes.rs
@@ -75,3 +75,95 @@ pub(super) const fn resource(
         changes,
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::DeclarationRecord;
+    use crate::state_store::{
+        TypedRequirementInput, TypedResourceKind, TypedRuleInput, TypedSourceInput,
+    };
+    use provenance_core::{Requirement, Rule, Source};
+
+    fn declaration_shape<T: DeclarationRecord>(
+        spec: &str,
+        declaration: &T::Declaration,
+    ) -> (TypedResourceKind, Vec<String>, Option<String>) {
+        let address = T::address(spec, declaration).unwrap();
+        (T::KIND, address.segments().to_vec(), T::parent(&address))
+    }
+
+    #[test]
+    fn declaration_records_define_their_resource_shape() {
+        let source = TypedSourceInput {
+            key: "policy".to_string(),
+            id: None,
+            name: "Policy".to_string(),
+            kind: "document".to_string(),
+            url: None,
+            reference: None,
+            supersedes: None,
+        };
+        let requirement = TypedRequirementInput {
+            key: "access".to_string(),
+            id: None,
+            statement: "The system controls access".to_string(),
+            description: None,
+            sources: Vec::new(),
+            refines: None,
+            depends_on: None,
+            supersedes: None,
+            spawned_by: None,
+        };
+        let rule = TypedRuleInput {
+            key: "deny".to_string(),
+            id: None,
+            address: None,
+            requirement: Some("access".to_string()),
+            requirements: Vec::new(),
+            statement: "The system denies unknown callers".to_string(),
+            name: None,
+            description: None,
+            resolution_ids: None,
+            implementation: None,
+        };
+
+        assert_eq!(
+            declaration_shape::<Source>("security", &source),
+            (
+                TypedResourceKind::Source,
+                vec![
+                    "security".to_string(),
+                    "source".to_string(),
+                    "policy".to_string()
+                ],
+                None,
+            )
+        );
+        assert_eq!(
+            declaration_shape::<Requirement>("security", &requirement),
+            (
+                TypedResourceKind::Requirement,
+                vec![
+                    "security".to_string(),
+                    "requirement".to_string(),
+                    "access".to_string(),
+                ],
+                None,
+            )
+        );
+        assert_eq!(
+            declaration_shape::<Rule>("security", &rule),
+            (
+                TypedResourceKind::Rule,
+                vec![
+                    "security".to_string(),
+                    "requirement".to_string(),
+                    "access".to_string(),
+                    "rule".to_string(),
+                    "deny".to_string(),
+                ],
+                Some("access".to_string()),
+            )
+        );
+    }
+}
