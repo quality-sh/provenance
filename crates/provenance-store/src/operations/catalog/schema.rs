acebff9abc873590849402d964b0a6a175ba8caa
Provenance CI snapshot <agent-ci@users.noreply.github.com>
2000-01-01 00:00:00 +0000

    Provenance CI snapshot

--- crates/provenance-store/src/operations/catalog/schema.rs
+++ crates/provenance-store/src/operations/catalog/schema.rs
@@ -1,11 +1,14 @@
 use super::{Operation, TargetAction};
-use provenance_core::protocol::{failure::OperationError, ResponseMeta};
+use provenance_core::protocol::failure::OperationError;
 use provenance_core::NodeType;
 use schemars::{
     generate::{Contract, SchemaSettings},
     JsonSchema,
 };
 use serde_json::{json, Value};
+
+mod envelope;
+pub(super) use envelope::{hide_bound_request_field, request_envelope, response_envelope};
 
 pub use super::schema_values::{
     parse_parameter_value, parse_schema_value, parse_schema_value_in, serialize_parameter_value,
@@ -322,50 +325,6 @@ fn statuses(mutates: bool, declared: &[u16]) -> Vec<u16> {
         .collect()
 }
 
-pub(super) fn response_envelope(mut payload: Value, kind: ResponseKind) -> Value {
-    if kind != ResponseKind::Items {
-        remove_response_metadata(
-            &mut payload,
-            &[
-                "protocol_version",
-                "operation",
-                "stamp",
-                "freshness_error",
-                "freshness_cause",
-                "limit",
-                "has_more",
-                "next_cursor",
-            ],
-        );
-    }
-    let defs = payload.as_object_mut().and_then(|o| o.remove("$defs"));
-    payload.as_object_mut().map(|o| o.remove("$schema"));
-    let mut meta = schema::<ResponseMeta>(Contract::Serialize);
-    namespace_defs(&mut meta, "ResponseMeta");
-    let meta_defs = meta.as_object_mut().and_then(|o| o.remove("$defs"));
-    meta.as_object_mut().map(|o| o.remove("$schema"));
-    let data = match kind {
-        ResponseKind::Items => {
-            json!({"type":"object","additionalProperties":false,"required":["items"],"properties":{"items":payload}})
-        }
-        ResponseKind::Resource | ResponseKind::Result => payload,
-    };
-    let mut result = json!({"type":"object","additionalProperties":false,"required":["data","meta"],"properties":{"data":data,"meta":meta}});
-    let mut merged = serde_json::Map::new();
-    if let Some(Value::Object(defs)) = defs {
-        merged.extend(defs);
-    }
-    if let Some(Value::Object(defs)) = meta_defs {
-        for (name, value) in defs {
-            merged.entry(name).or_insert(value);
-        }
-    }
-    if !merged.is_empty() {
-        result["$defs"] = Value::Object(merged);
-    }
-    result
-}
-
 pub(super) fn property_schema(root: &Value, path: &[&str]) -> Value {
     let mut current = root;
     for name in path {
@@ -389,76 +348,6 @@ fn resolve_schema<'a>(root: &'a Value, schema: &'a Value) -> &'a Value {
         .and_then(|reference| reference.strip_prefix("#/$defs/"))
         .and_then(|name| root.get("$defs").and_then(|defs| defs.get(name)))
         .unwrap_or(schema)
-}
-
-pub(super) fn namespace_defs(value: &mut Value, prefix: &str) {
-    match value {
-        Value::Object(object) => {
-            if let Some(Value::String(reference)) = object.get_mut("$ref") {
-                if let Some(name) = reference.strip_prefix("#/$defs/") {
-                    *reference = format!("#/$defs/{prefix}{name}");
-                }
-            }
-            if let Some(Value::Object(defs)) = object.remove("$defs") {
-                object.insert(
-                    "$defs".into(),
-                    Value::Object(
-                        defs.into_iter()
-                            .map(|(name, schema)| (format!("{prefix}{name}"), schema))
-                            .collect(),
-                    ),
-                );
-            }
-            for child in object.values_mut() {
-                namespace_defs(child, prefix);
-            }
-        }
-        Value::Array(array) => {
-            for child in array {
-                namespace_defs(child, prefix);
-            }
-        }
-        _ => {}
-    }
-}
-
-pub(super) fn request_envelope(mut request: Value) -> Value {
-    let defs = request.as_object_mut().and_then(|o| o.remove("$defs"));
-    request.as_object_mut().map(|o| o.remove("$schema"));
-    let mut result = json!({"type":"object","additionalProperties":false,"required":["data"],"properties":{"data":request}});
-    if let Some(defs) = defs {
-        result["$defs"] = defs;
-    }
-    result
-}
-
-pub(super) fn hide_bound_request_field(request: &mut Option<Value>, field: &str) {
-    let Some(data) = request
-        .as_mut()
-        .and_then(|schema| schema.pointer_mut("/properties/data"))
-    else {
-        return;
-    };
-    data.pointer_mut("/properties")
-        .and_then(Value::as_object_mut)
-        .map(|properties| properties.remove(field));
-    if let Some(required) = data.pointer_mut("/required").and_then(Value::as_array_mut) {
-        required.retain(|name| name.as_str() != Some(field));
-    }
-}
-
-fn remove_response_metadata(value: &mut Value, fields: &[&str]) {
-    let Some(object) = value.as_object_mut() else {
-        return;
-    };
-    if let Some(Value::Object(properties)) = object.get_mut("properties") {
-        for field in fields {
-            properties.remove(*field);
-        }
-    }
-    if let Some(Value::Array(required)) = object.get_mut("required") {
-        required.retain(|field| !fields.iter().any(|name| field == *name));
-    }
 }
 
 pub fn definitions() -> &'static [Definition] {
