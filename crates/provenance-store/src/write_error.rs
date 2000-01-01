d3624fb7ee5ca7c7dd8540971d1061407ce56ebe
Provenance CI snapshot <agent-ci@users.noreply.github.com>
2000-01-01 00:00:00 +0000

    Provenance CI snapshot

--- crates/provenance-store/src/write_error.rs
+++ crates/provenance-store/src/write_error.rs
@@ -25,6 +25,11 @@ pub enum WriteFailure {
     },
     MissingReference,
     ResourceNotFound,
+    DiscussionVersionConflict,
+    DiscussionMembershipMismatch,
+    DiscussionClosed,
+    DiscussionResolved,
+    DiscussionIntentChanged,
     StatementRejected {
         diagnostics: Vec<TypedSpecDiagnostic>,
     },
@@ -129,6 +134,10 @@ impl WriteError {
             WriteFailure::RecordOwnershipConflict
             | WriteFailure::AlreadyExists
             | WriteFailure::OwnershipConflict { .. }
+            | WriteFailure::DiscussionVersionConflict
+            | WriteFailure::DiscussionClosed
+            | WriteFailure::DiscussionResolved
+            | WriteFailure::DiscussionIntentChanged
             | WriteFailure::AlreadyComplete => 409,
             _ => 400,
         }
