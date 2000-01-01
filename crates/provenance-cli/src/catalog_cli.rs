3686a8c681b0316a04c9216d56ce3e137c2d0805
Provenance CI snapshot <agent-ci@users.noreply.github.com>
2000-01-01 00:00:00 +0000

    Provenance CI snapshot

--- crates/provenance-cli/src/catalog_cli.rs
+++ crates/provenance-cli/src/catalog_cli.rs
@@ -342,14 +342,21 @@ fn input(
             }
         } else if let Some(request_schema) = &definition.request_schema {
             let field = flag.replace('-', "_");
-            let schema = body_field_schema(request_schema, &field)
+            let wire_field = definition
+                .registration
+                .request
+                .argument_aliases
+                .iter()
+                .find(|alias| alias.argument == field)
+                .map_or(field.as_str(), |alias| alias.field);
+            let schema = body_field_schema(request_schema, wire_field)
                 .ok_or_else(|| anyhow::anyhow!("unknown body field: --{flag}"))?;
             if value.starts_with('[') || value.starts_with('{') {
                 anyhow::bail!("arrays and objects must come from --stdin");
             }
             let parsed = catalog::parse_schema_value_in(request_schema, schema, value)
                 .map_err(|_| anyhow::anyhow!("invalid value for --{flag}"))?;
-            data.insert(field, parsed);
+            data.insert(wire_field.to_owned(), parsed);
         } else {
             query.insert(flag.replace('-', "_"), value.clone());
         }
@@ -404,7 +411,6 @@ fn input(
         data.entry("links").or_insert_with(|| json!([]));
     }
     if definition.name == "create-question" {
-        rename_scalar(&mut data, "method", "resolution_method");
         data.entry("status").or_insert_with(|| json!("open"));
         data.entry("links").or_insert_with(|| json!([]));
     }
@@ -441,12 +447,6 @@ fn body_field_schema<'a>(request: &'a Value, field: &str) -> Option<&'a Value> {
 fn move_scalar_to_singleton(data: &mut Map<String, Value>, source: &str, target: &str) {
     if let Some(value) = data.remove(source) {
         data.insert(target.to_owned(), json!([value]));
-    }
-}
-
-fn rename_scalar(data: &mut Map<String, Value>, source: &str, target: &str) {
-    if let Some(value) = data.remove(source) {
-        data.insert(target.to_owned(), value);
     }
 }
 
---
+++ crates/provenance-store/src/operations/catalog/routes/finalize.rs
@@ -0,0 +1,49 @@
+use super::{schema, BodyBinding, Definition, SelectorBinding};
+
+pub(super) fn request_schemas(definitions: &mut [Definition]) {
+    for definition in definitions {
+        let registration = &definition.registration;
+        let mut fields = registration
+            .request
+            .path
+            .iter()
+            .map(|binding| binding.field)
+            .collect::<Vec<_>>();
+        fields.extend(registration.request.scope_field);
+        fields.extend(
+            registration
+                .request
+                .parent
+                .iter()
+                .map(|binding| binding.field),
+        );
+        fields.extend(
+            registration
+                .request
+                .selector
+                .iter()
+                .map(|binding| match binding {
+                    SelectorBinding::Discussion { field, .. }
+                    | SelectorBinding::Legacy { field, .. } => *field,
+                }),
+        );
+        fields.extend(
+            registration
+                .controls
+                .headers
+                .iter()
+                .map(|binding| binding.field),
+        );
+        if !matches!(
+            registration.request.body,
+            BodyBinding::Direct | BodyBinding::Null
+        ) {
+            fields.push("action");
+        }
+        fields.sort_unstable();
+        fields.dedup();
+        for field in fields {
+            schema::hide_bound_request_field(&mut definition.request_schema, field);
+        }
+    }
+}
