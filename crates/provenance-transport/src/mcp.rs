3f86d57efff4c1fa1c755e10068bc356ed20e724
Provenance CI snapshot <agent-ci@users.noreply.github.com>
2000-01-01 00:00:00 +0000

    Provenance CI snapshot

--- crates/provenance-transport/src/mcp.rs
+++ crates/provenance-transport/src/mcp.rs
@@ -54,6 +54,9 @@ impl ServerHandler for StatementHost {
         if crate::porcelain::search_is_available(self) {
             tools.push(crate::porcelain::search_tool());
         }
+        if crate::porcelain::api_is_available(self) {
+            tools.push(crate::porcelain::api_tool());
+        }
         if self.check_port().is_some() {
             tools.push(crate::porcelain::check_tool());
         }
@@ -75,6 +78,34 @@ impl ServerHandler for StatementHost {
         }
         if request.name == "search" && crate::porcelain::search_is_available(self) {
             return call_search(self, request.arguments).await;
+        }
+        if request.name == "api" {
+            if !crate::porcelain::api_is_available(self) {
+                return Err(ErrorData::new(
+                    ErrorCode::METHOD_NOT_FOUND,
+                    "Unknown tool",
+                    None,
+                ));
+            }
+            let _admission = match self.admit() {
+                Ok(permit) => permit,
+                Err(failure) => return Ok(error(failure)),
+            };
+            let arguments = request.arguments.unwrap_or_default();
+            if serde_json::to_vec(&arguments)
+                .map_err(|_| ErrorData::internal_error("Cannot encode input", None))?
+                .len()
+                > MAX_BODY_BYTES
+            {
+                return Ok(error(ErasedFailure::new(
+                    None,
+                    OperationFailure::InvalidInput {
+                        field: None,
+                        reason: InvalidInputReason::TooLarge,
+                    },
+                )));
+            }
+            return Ok(crate::porcelain::call_api(self, Some(arguments)).await);
         }
         if let Some(action) = crate::porcelain::Action::DISCUSSION
             .into_iter()
