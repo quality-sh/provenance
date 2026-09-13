1adc18821a62c29081bfab617e6cd5e9c046ca9e
Ben <nasraouiprojects@gmail.com>
2026-09-14 09:00:33 +1000

    provenance-yvrw: enforce Rust CRAP quality gate

---
+++ .github/scripts/fixtures/crap_gate/failing/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn complex_uncovered(value: u8) -> u8 {
+    if value == 0 {
+        0
+    } else if value == 1 {
+        1
+    } else if value == 2 {
+        2
+    } else if value == 3 {
+        3
+    } else if value == 4 {
+        4
+    } else {
+        5
+    }
+}
